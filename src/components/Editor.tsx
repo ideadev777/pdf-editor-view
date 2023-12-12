854f2df7afe00b177f426a3e16af78813d813592
unknown <seniorarm99@gmail.com>
2023-12-11 16:56:21 -0800

    Feature: wheel event

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -134,6 +134,11 @@ const Editor = (props: Props) => {
   const handleScroll = (e:any) => {
     console.log('scrolled', e)
   }
+
+  const handleWheel = (e:any) => {
+    console.log('handleWheel', e)
+  }
+
   const handleClick = (e: any) => {
     if(selected) {
       const pageCanvas = coordiate.current.parentNode
@@ -490,7 +495,7 @@ const Editor = (props: Props) => {
       options={options}
       file={source}>
       <div style={{ height: 'calc(100vh - 116px)',  overflow: 'auto', backgroundColor: 'lightgray', display: 'flex', justifyContent: 'center', paddingTop: '20px', paddingBottom: '20px' }}>
-        <Page scale={scale} canvasRef={drop} onKeyPress={handleKeyPress} onScroll={handleScroll} onClick={handleClick} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} pageNumber={currentPage} renderAnnotationLayer={false} renderTextLayer={false}>
+        <Page scale={scale} canvasRef={drop} onKeyPress={handleKeyPress} onScroll={handleScroll} onWheel={handleWheel} onClick={handleClick} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} pageNumber={currentPage} renderAnnotationLayer={false} renderTextLayer={false}>
           <div id='elementToCapture' style={{ position: 'absolute', top: 0, left: 0, transformOrigin: 'top left', transform: `scale(${scale})` }} className="viewport">
             {selected && renderTempField()}
             {renderCurrentFields()}
